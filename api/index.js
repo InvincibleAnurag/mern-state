7888045cd588b72724a94f193d8f6ddd8ffe0e20
Anurag Pandey <invincibleanurag5@gmail.com>
2024-07-10 17:10:14 +0530

    created test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+// import path from 'path';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 
@@ -15,3 +17,14 @@ const app = express();
 app.listen(3000,()=>{
      console.log('Server is runnning on port 3000!!!');
 })
+
+// app.get('/test', (req, res) => {
+//     const filePath = path.join(process.cwd(),'api', 'doc.txt');
+//     res.sendFile(filePath);
+// });
+
+// app.get('/test', (req,res)=>{
+//     res.send("API is working!!");
+// })
+
+app.use('/api/user', userRouter);
