93cb3b759d0bcbd3fd2c028333fa62cf64604053
Anurag Pandey <invincibleanurag5@gmail.com>
2024-07-23 11:45:25 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice";
 import {
   getStorage,
   getDownloadURL,
@@ -66,6 +66,23 @@ export default function Profile() {
    const handleChange = (e) => {
        setFormData({...formData, [e.target.id] : e.target.value})
    }
+
+   const handleDeleteUser = async () => {
+      try {
+        dispatch(deleteUserStart());
+        const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+           method:'DELETE',
+        });
+        const data = await res.json();
+        if(data.success === false) {
+           dispatch(deleteUserFailure(data.message));
+           return;
+        }
+        dispatch(deleteUserSuccess(data));
+      } catch (error) {
+          dispatch(deleteUserFailure(error.message))
+      }
+   };
 
    const handleSubmit = async (e) => {
       e.preventDefault();
@@ -148,10 +165,10 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
-        <p className="text-red-700 mt-5">{error ? error : ""}</p>
+        {/* <p className="text-red-700 mt-5">{error ? error : ""}</p> */}
         <p className="text-green-700 mt-5">{updateSuccess ? "User is updated successfully!": ""}</p>
     </div>
   );
